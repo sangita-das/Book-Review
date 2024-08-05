bb9b6b5405a8c9fc58ca55e4be3e40738f632e8f
Sangita <mistymoni5050@gmail.com>
2024-08-06 00:13:22 +0600

    added all

--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -7,6 +7,21 @@ const BookList = () => {
     const books = useLoaderData();
 
     const [readBooks, setReadBooks] = useState([]);
+    const [displayBooks, setDisplayBooks] = useState([]);
+
+    const handleBooksFilter = filter => {
+        if(filter === 'rating'){
+            setDisplayBooks(readBooks);
+        }
+        else if ( filter === 'totalPages'){
+            const totalPages = readBooks.filter(book => book. Number_of_pages === 'totalPages')
+            setDisplayBooks(totalPages);
+        }
+        else if(filter === 'yearOfPublishing'){
+            const year_of_publishing = readBooks.filter(book => book. Year_of_publishing === 'yearOfPublishing');
+            setDisplayBooks(year_of_publishing)
+        }
+    }
 
 
     useEffect( ()  => {
@@ -27,8 +42,9 @@ const BookList = () => {
 
 
             setReadBooks(readBooks);
+            setDisplayBooks(readBooks);
         }
-    }, [])
+    }, [books])
     return (
         <div>
             <h2 className="text-2xl">This is book list page: {readBooks.length}</h2>
@@ -38,15 +54,15 @@ const BookList = () => {
             <details className="dropdown">
   <summary className="btn m-1">open or close</summary>
   <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-    <li><a>Rating</a></li>
-    <li><a>Number of pages</a></li>
-    <li> <NavLink>Publisher year </NavLink> </li>
+    <li onClick = { () => handleBooksFilter('rating')}><a>Rating</a></li>
+    <li onClick={() => handleBooksFilter ('totalPages')}><a>Number of pages</a></li>
+    <li onClick = {() => handleBooksFilter('yearOfPublishing')}> <NavLink>Publisher year </NavLink> </li>
   </ul>
 </details>
 
             <ul>
                 {
-                    readBooks.map(book => <li
+                    displayBooks.map(book => <li
                     key={book.bookId}
                     >
                     <span>{book.bookName} {book.author} : published year: {book. yearOfPublishing}</span>
